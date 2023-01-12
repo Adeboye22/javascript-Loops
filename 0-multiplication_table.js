3f899887827f1abc941eb771e7a4306b9d5a2028
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2023-01-12 05:05:50 +0100

    nested for loops

---
+++ 0-multiplication_table.js
@@ -0,0 +1,8 @@
+// using for loop create a variable i
+for (var i=0; i<13; i++){
+	console.log(i);
+	//using a nested loop create a variable j
+	for (var j=0; j<13; j++){
+		console.log(i + " times " + j + " equals to " + i * j);
+	};
+}
