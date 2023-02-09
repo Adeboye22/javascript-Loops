ee730d3a6fdf506f813ed56d692bbe327b2d1c7e
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2023-02-09 23:33:26 +0100

    loops

---
+++ forLoops.js
@@ -0,0 +1,30 @@
+/*
+ * Task 1
+ * 
+for(var i = -9; i < 19; i++){
+	console.log(i)
+}
+
+
+//task 2
+
+for(var i = 10; i < 40; i+=2) {
+	console.log(i)
+}
+
+
+//task 3
+
+for(var i = 301; i < 333; i+=2) {
+	console.log(i)
+}
+*/
+
+//task 4
+
+for(var i = 5; i< 50; i++) {
+	//write a conditional statement
+	if(i % 5 == 0 && i % 3 == 0){
+		console.log(i)
+	}
+}
